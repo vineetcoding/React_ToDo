d50da52c4e2ed0084314b0e1e09916ecdec56d08
vineetcoding <114997519+vineetcoding@users.noreply.github.com>
2023-01-18 17:40:42 +0530

    ToDo

---
+++ src/assTodo.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import todoList from "./todoList";
+
+const App = () => {
+
+  const [inputList, setInputList] = useState("")
+  const [Items, setItems] = useState([])
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value)
+  }
+  const listOfItem = () => {
+    setItems((oldItems) =>{
+      return [...oldItems,inputList]
+    });
+    setInputList("")
+   }
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>ToDo List</h1>
+          <input type="text" placeholder='Add a item' value={inputList} onChange={itemEvent} />
+
+          <button onClick={listOfItem}>ADD</button>
+          <ol>
+            {/* <li> {inputList}</li> */}
+
+            {Items.map((intemval) => {
+             return <todoList text={intemval} />
+            })}
+          </ol>
+        </div>
+
+      </div>
+    </>
+  )
+}
+export default App;
